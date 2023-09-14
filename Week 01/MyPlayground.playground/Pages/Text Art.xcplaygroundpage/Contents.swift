6ff03e4f24a999964984c85920438186ee8a1064
Erika <yx2165@nyu.edu>
2023-09-14 09:17:41 -0400

    Week 01

---
+++ Week 01/MyPlayground.playground/Pages/Text Art.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+func generateASCIIArt() {
+    let line1 = "▕╮╭┻┻╮╭┻┻╮╭▕╮╲"
+    let line2 = "▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏"
+    let line3 = "▕╭┻┻┻┛┗┻┻┛ ╰▏"
+    let line4 = "▕╰━━━┓┈┈┈╭╮▕╭╮▏"
+    let line5 = "▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏"
+    let line6 = "▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏"
+
+    let asciiArt = "\(line1)\n\(line2)\n\(line3)\n\(line4)\n\(line5)\n\(line6)"
+
+    print(asciiArt)
+}
+
+generateASCIIArt()
+
+
+//: [Next](@next)
