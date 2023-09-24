1a6c126f7f2de7e107365afa3935f177dd1612c7
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-09-24 13:15:54 -0400

    Create Contents.swift

---
+++ Week 02/Week 02.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+// https://developer.apple.com/documentation/swift/array/randomelement()
+
+let names = ["Zoey", "Chloe", "Amani", "Amaia"]
+let randomName = names.randomElement()!
+
+//: [Next](@next)
